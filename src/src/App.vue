addfc339a2a5d54c54e5e7d363f2012bd38cf355
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-09-08 16:41:18 +0200

    Code cleanup, added ContactInformation

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ContactInformation />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ContactInformation } from "./components";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ContactInformation,
+  },
+};
 </script>
 
-<style>
-#app {
+<style src="./assets/css/style.css" lang="css"></style>
+
+<style scoped>
+.container {
+  background: linear-gradient(
+    to right,
+    var(--high-contrast-color) var(--panel-size),
+    var(--low-contrast-color) var(--panel-size) calc(var(--panel-size) * 2)
+  );
+  border-top: var(--space-unit) solid var(--low-contrast-color);
+  height: 100vh;
+  padding: var(--space-unit);
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/src/components/ContactInformation.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <h2>Cooperation</h2>
+
+    <ul>
+      <li>
+        <a href="mailto:monique@ap.com">monique@ap.com</a>
+      </li>
+      <li>
+        <a href="tel:+46812031580">+46 8 120 315 80</a>
+      </li>
+      <li>
+        <p>
+          inst:
+          <a href="http://" target="_blank" rel="noopener noreferrer">@moniqueb</a>
+        </p>
+      </li>
+      <li>
+        <p>
+          behance:
+          <a href="http://" target="_blank" rel="noopener noreferrer">moniqueb</a>
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ContactInformation",
+};
+</script>
+
+<style scoped>
+h2 {
+  margin: 0;
+}
+p {
+  margin: 0;
+}
+ul {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(auto, 15rem));
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
