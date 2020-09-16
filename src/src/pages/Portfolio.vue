548322598ccb302d6633851673bf1961b5f2836a
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-09-16 19:39:44 +0200

    Added portfolio view

---
+++ src/src/pages/Portfolio.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h2>Portfolio</h2>
+    <p>
+      In here, you'll find a selection of my previous work.
+      <br />If you want to see more, do not hesitate in contacting me!
+    </p>
+
+    <ul class="ul">
+      <li><img class="img" src="../assets/img/img-1.jpeg" alt="Portfolio image" /></li>
+      <li><img class="img" src="../assets/img/img-2.jpeg" alt="Portfolio image" /></li>
+      <li><img class="img" src="../assets/img/img-3.jpeg" alt="Portfolio image" /></li>
+      <li><img class="img" src="../assets/img/img-4.jpeg" alt="Portfolio image" /></li>
+      <li><img class="img" src="../assets/img/img-5.jpeg" alt="Portfolio image" /></li>
+      <li><img class="img" src="../assets/img/img-6.jpeg" alt="Portfolio image" /></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default { name: "Portfolio" };
+</script>
+
+<style scoped>
+.img {
+  max-width: 20vw;
+}
+
+.ul {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  margin: 1rem 0;
+}
+</style>
--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import { About, Contact, Home, PageNotFound } from '../pages';
+import { About, Contact, Home, PageNotFound, Portfolio } from '../pages';
 
 Vue.use(Router);
 
@@ -10,7 +10,7 @@ export default new Router({
     { path: '/', component: Home, },
     { path: '/about', component: About, },
     { path: '/contact', component: Contact, },
-    // { path: '/portfolio', component: Portfolio, },
+    { path: '/portfolio', component: Portfolio, },
     { path: '*', component: PageNotFound }
   ],
 });
