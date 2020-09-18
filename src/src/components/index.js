068a5f09c855735ea60e7746db20c173978871ef
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-09-18 10:22:42 +0200

    Published demo version

--- src/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Carousel } from './Carousel';
-export { default as ContactInformation } from './ContactInformation';
-export { default as HeaderPanel } from './HeaderPanel';
-export { default as ModalImage } from './ModalImage';
-export { default as SiteNavigation } from './SiteNavigation';
