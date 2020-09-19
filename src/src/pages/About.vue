d691d02db4965806020221f4b9efe676b7bc1e3f
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-09-19 18:23:32 +0200

    Added About page

---
+++ src/src/pages/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <article>
+      <h2>Welcome!</h2>
+
+      <p>
+        <br />I'm Anastasia, a passionate photographer.
+        <br />
+        <br />I've been part of the fashion industry for over 20 years now, as well as a hobbist landscape lover.
+        <br />I try to find beauty wherever I go and immortalize it through the lens of my camera, a faithful companion always at my side.
+      </p>
+    </article>
+
+    <img
+      class="img"
+      src="../assets/img/img-about.jpeg"
+      alt="Myself, looking at you through the lens of a camera"
+    />
+  </section>
+</template>
+
+<script>
+export default { name: "About" };
+</script>
+
+<style scoped>
+.img {
+  margin: auto;
+  width: 100%;
+}
+
+.section {
+  background-color: var(--high-contrast-color);
+}
+</style>
